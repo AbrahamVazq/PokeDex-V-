118581358ceb9a54aef8a6240a54c19b829cfe6e
Moises Abraham Vazquez Perez <mavazquezp@elektra.com.mx>
2023-04-24 20:38:56 -0600

    [UPDATe] Update files

---
+++ PokeDex(V)/Services/PokeServices.swift
@@ -0,0 +1,44 @@
+//
+//  PokeServices.swift
+//  PokeDex(V)
+//
+//  Created by Moises Abraham Vazquez Perez on 13/02/23.
+//
+
+import Foundation
+import Alamofire
+
+
+final class PokeSerivesProvider {
+    
+    //MARK: - B L O C K
+    public typealias blkGetAllPokemon = (PokedexNational?, AFError?) -> Void
+    
+    
+    //MARK: - V A R I A B L E S
+    static let shared = PokeSerivesProvider()
+    
+    private let urlAllPokemon = "https://pokeapi.co/api/v2/pokedex/1/"
+    private let okStatus = 200 ... 299
+    
+    func getAllPokemon(withHandler: @escaping blkGetAllPokemon){
+        
+        let url = urlAllPokemon
+        AF.request(url, method: .get).validate(statusCode: okStatus).responseDecodable(of: PokedexNational.self) { response in
+            print(response)
+            switch response.result {
+            case .success(let value):
+                withHandler(value, nil)
+            case .failure(let error):
+                if response.response?.statusCode == 200 {
+                    withHandler(nil, nil)
+                } else {
+                    withHandler(nil, error)
+                }
+            }
+        }
+    }
+    
+    
+    
+}
