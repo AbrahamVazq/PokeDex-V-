db41c33bf4d7412c165cf7a3a23330993b996831
Moises Abraham Vazquez Perez <mavazquezp@elektra.com.mx>
2023-04-25 15:27:48 -0600

    [UPDATE] Update Protocols Guide

--- PokeDex(V)/Modules/MainView/mainAllPokemonProtocols.swift
+++ PokeDex(V)/Modules/MainView/mainAllPokemonProtocols.swift
@@ -1,32 +1,103 @@
 //  mainAllPokemonProtocols.swift
 //  PokeDex(V)
-//
 //  Created by Moises Abraham Vazquez Perez on 13/02/23.
-//  
 //  ViperTemplate v.0.0.1 - (2023, NS-Bionick Development Team)
 
 import Foundation
+/**
+ Sugerencias en el llamado de Funciones de Vista a Presenter
+ :condiciones:
+ 
+ Si queremos ir a otro modulo VIPER
+ 
+    * goTo___________( )
+    * goTo___________( _ : )
+ 
+ Si queremos ir por datos o hacer un consumo
+    * viewDidLoad()
+    * getTo___________( )
+    * getTo___________( _ : )
+ */
 
 // MARK: VIEW -> PRESENTER
 protocol mainAllPokemon_ViewToPresenterProtocol: AnyObject {
-	var view: mainAllPokemon_PresenterToViewProtocol? { get set }
-	var interactor: mainAllPokemon_PresenterToInteractorProtocol? { get set }
-	var router: mainAllPokemon_PresenterToRouterProtocol? { get set }
+    var view: mainAllPokemon_PresenterToViewProtocol? { get set }
+    var interactor: mainAllPokemon_PresenterToInteractorProtocol? { get set }
+    var router: mainAllPokemon_PresenterToRouterProtocol? { get set }
 }
+
+//MARK: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/**
+ Sugerencias en el llamado de Funciones de  Presenter a Interactor
+ :condiciones:
+ 
+ Tratemos que la funcion se llame igual que desde la que viene en la vista al Presenter,
+ pero en esta ocacion agregemosle el subfijo: 'ToInteractor', para saber que vamos
+ del presenter al iintectartor
+ 
+    * get___________ToInteractor( )
+    * getTo___________ToInteractor( )
+ 
+    * get___________ToInteractor( _ : )
+    * getTo___________ToInteractor( _ : )
+ 
+ */
 
 // MARK: PRESENTER -> INTERACTOR
 protocol mainAllPokemon_PresenterToInteractorProtocol: AnyObject {
     var presenter: mainAllPokemon_InteractorToPresenterProtocol? { get set }
 }
 
+//MARK: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+/**
+ Sugerencias en el llamado de Funciones de Vista a Presenter
+ :condiciones:
+ 
+ Tratemos que la funcion se llame igual que desde la que viene en la vista al Presenter,
+ pero en esta ocacion agregemosle el subfijo: 'FromInteractor' con los parametros que correspondan,
+ para saber que vamos del Interactor  al Presenter
+ 
+    * get___________FromInteractor( )
+    * getTo___________FromInteractor( )
+ 
+    * get___________FromInteractor(_ :)
+    * getTo___________FromInteractor(_ :)
+ */
+
 // MARK: INTERACTOR -> PRESENTER
 protocol mainAllPokemon_InteractorToPresenterProtocol: AnyObject {
 }
+
+//MARK: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/**
+ Sugerencias en el llamado de Funciones de Vista a Presenter
+ :condiciones:
+ 
+ Tratemos de usar la funcion updateView(_ :), con los parametros que correspondan, para
+ saber que vamos del Presenter a la Vista.
+ 
+    * updateView(_: )
+ 
+ */
 
 // MARK: PRESENTER -> VIEW
 protocol mainAllPokemon_PresenterToViewProtocol: AnyObject {
     var presenter: mainAllPokemon_ViewToPresenterProtocol? { get set }
 }
+
+//MARK: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/**
+ Sugerencias en el llamado de Funciones de Vista a Presenter
+ :condiciones:
+ 
+ Si queremos ir a otro modulo VIPER, usemos goTo_________( _ : ), con parametros si es necesario para
+ asegurar que vamos del Presenter a el Router de otro Modulo VIPER.
+ 
+    * goTo___________( _ : )
+ */
 
 // MARK: PRESENTER -> ROUTER
 protocol mainAllPokemon_PresenterToRouterProtocol: AnyObject {
