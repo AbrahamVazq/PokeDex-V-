b7db78d23f8644a89273a87abfb2f819217d0d67
Abraham Vazquez <mabraham.vazquez@gmail.com>
2023-05-08 15:13:56 -0600

    [UPDATE] update main view presentation * add view for 3 steps to evolution * add view for 2 steps to evolution

---
+++ PokeDex(V)/View/Evolution2StepsView/Evolution2StepsView.swift
@@ -0,0 +1,9 @@
+//  Evolution2StepsView.swift
+//  PokeDex(V)
+//  Created by Abraham Vazquez on 08/05/23.
+
+import UIKit
+
+class Evolution2StepsView: UIView {
+    
+}
---
+++ PokeDex(V)/View/Evolution3StepsView/Evolution3StepsView.swift
@@ -0,0 +1,12 @@
+//
+//  Evolution3StepsView.swift
+//  PokeDex(V)
+//
+//  Created by Abraham Vazquez on 08/05/23.
+//
+
+import UIKit
+
+class Evolution3StepsView: UIView {
+    
+}
